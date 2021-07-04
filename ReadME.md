c4956138eeb2a75cc2eae7d0c5b11084003f3408
Vahe Sevachyan <vscode13@gmail.com>
2021-07-03 19:08:46 -0700

    Form-Validator
    Form Validator for the front end.

---
+++ ReadME.md
@@ -0,0 +1,7 @@
+# Form Validator
+
+> This is a form validator for the front end, built with HTML and CSS.
+
+> _screen shot sample below!_
+
+> ![](MD-images/1.png?raw=true) > ![](MD-images/3.png?raw=true) > ![](MD-images/2.png?raw=true)
---
+++ app.js
@@ -0,0 +1,58 @@
+const form = document.getElementById("form");
+const password1EL = document.getElementById("password1");
+const password2EL = document.getElementById("password2");
+const messageContainer = document.querySelector(".message-container");
+const message = document.getElementById("message");
+
+let isValid = false;
+let passwordsMatch = false;
+
+function validateForm(){
+    isValid = form.checkValidity();
+    if(!isValid){
+    message.textContent = "Please fill out all fields.";
+    message.style.color = "red";
+    messageContainer.style.borderColor = "red"
+    return;
+    }
+    //checking to see if passwords match
+    if(password1EL.value === password2EL.value){
+        passwordsMatch = true;
+        password1EL.style.borderColor = "green";
+        password2EL.style.borderColor = "green";
+    }else{
+        passwordsMatch = false;
+        message.textContent = "Please make sure your passwords match."
+        message.style.color = "red";
+        messageContainer.style.borderColor = "red";
+        password1EL.style.borderColor = "red";
+        password2EL.style.borderColor = "red";
+        return;
+    }
+    //if form is valid and passwords match
+    if(isValid && passwordsMatch){
+        message.textContent = "Successfully Registered!";
+        message.style.color = "green";
+        messageContainer.style.borderColor = "green";
+    }
+}
+function storeFormData(){
+    const user = {
+        name: form.name.value,
+        phone: form.phone.value,
+        email: form.email.value,
+        website: form.website.value,
+        password: form.password.value
+    }
+    console.log(user);
+}
+
+function processFormData (e){
+    e.preventDefault();
+    validateForm();
+    if(isValid && passwordsMatch){
+        storeFormData();
+    }
+}
+
+form.addEventListener("submit", processFormData);
